36f90da950cedd5d6bf4872dfbb75d51d5cd8420
WooSangWook <raincoat@kakao.com>
2024-04-12 11:30:41 +0900

    Feat: Add quasar

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,15 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
 
-createApp(App).mount('#app')
+import App from "./App.vue";
+
+import { Quasar } from "quasar";
+import "@quasar/extras/material-icons/material-icons.css";
+import "quasar/src/css/index.sass";
+
+const app = createApp(App);
+
+app.use(Quasar, {
+  plugins: {},
+});
+
+app.mount("#app");
